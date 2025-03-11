1cd414d4b466153d1fb64e887ef8e922a7f97fc8
ThunderR97 <arthur3730@hotmail.no>
2025-03-11 09:48:58 +0100

    Oppdatert logg inn

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+window.onload = function() {
+    if (localStorage.getItem("LoggedIn") === "true") {
+        document.getElementById("admin_welcome").innerHTML = "Velkommen Administrator";
+        document.getElementById("login").innerHTML = "";
+    } else {
+        document.getElementById("admin_welcome").innerHTML = "Velkommen Gjest";
+        document.getElementById("post_articles").innerHTML = "";
+    }
+}
+
 let login_username = document.getElementById("login_username");
 let login_password = document.getElementById("login_password");
 let login_output = document.getElementById("login_output");
@@ -12,8 +22,9 @@ function AdminLogin() {
     // Brukernavn og passord laget av registrering
     if (login_attempts === 8) {
         login_output.innerHTML = "Du har nådd grensen av antall forsøk. Prøv igjen senere.";
-    } else if (login_username.value === "KorkenLord" && login_password.value === "LegoMan3251") {
+    } else if (login_username.value === "test" && login_password.value === "test") {
         login_output.innerHTML = "Hei Administrator";
+        localStorage.setItem("LoggedIn", "true");
     } else {
         login_output.innerHTML = "Feil brukernavn eller passord";
         login_attempts++;
@@ -32,5 +43,28 @@ function AdminLogin() {
         login_output.innerHTML = `Du må fylle alle feltene`;
     } else {
         password_label.innerHTML = "Passord:";
+    }
+}
+
+function AdminLogout() {
+    if (localStorage.getItem("LoggedIn") === "true") {
+        localStorage.setItem("LoggedIn", "false");
+    }
+}
+
+function PostArticle() {
+    const article = document.getElementById("the_article");
+    const art_image = document.getElementById("imageSelected").files[0];
+    const art_text = document.getElementById("articleText");
+
+    if (art_image) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            article.innerHTML = `<img src="${e.target.result}" style="max-width:500px; height:auto;">
+            <p>${art_text.value}</p>`;
+        };
+        reader.readAsDataURL(art_image);
+    } else {
+        article.innerHTML = `<p>${art_text.value}</p>`;
     }
 }
